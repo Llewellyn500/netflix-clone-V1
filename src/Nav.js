0fd964da61d08d3f35c9ebfbfbd25448ca53bade
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 17:10:25 +0000

    update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 function Nav() {
   return (
-      <div>
-      
+      <div className="nav">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+          alt="Netflix Logo"
+        />
+        
+        <img 
+          className="nav__avatar"
+          src="https://pbs.twimg.com/profile_images/12401199904155"
+          alt="Netflix Logo"
+        />
       </div>
-    )
+  );
 }
 
 export default Nav
