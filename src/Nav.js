f657d3ab2174dee55b0c79036abc362314677803
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 17:41:02 +0000

    update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import Nav from "./Nav";
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
 function Nav() {
+    const [show, handleShow] = useState(false);
+    
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+        } else handleShow(false);
+      });
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    }, []);
+    
   return (
-      <div className="nav">
+      <div className={`nav ${show && "nav__black"}`}>
         <img
           className="nav_logo"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
