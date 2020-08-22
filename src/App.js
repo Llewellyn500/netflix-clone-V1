279b67bcc4f17d6639ded1473109f05c785a5a77
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:35:21 +0000

    added the rest of the movies

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       <h1>Building netflix clone today</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Hoeeoe Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
