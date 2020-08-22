540b874fe77f3475fa3b814181dcdf19ef650b1e
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 16:07:19 +0000

    update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
