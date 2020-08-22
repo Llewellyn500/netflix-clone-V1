7b220655907f9d7a2b2e3f37092238ab8826f0e0
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 12:25:15 +0000

    update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEFfect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
 function Banner() {
