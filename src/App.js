8b15ba7887b0667b1f2ff2ebf1d34c095fd5b7ea
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:58:46 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
-
+import Banner from './Banner'
 function App() {
   return (
     <div className="App">
-     
+     <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
