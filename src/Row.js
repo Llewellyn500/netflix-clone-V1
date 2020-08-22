dc3163401f5946ee56b8ea592728428f09b6cd99
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:52:22 +0000

    updated Row.js

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   
   // a snippet of code which runs based on a specific condition
@@ -27,8 +27,8 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img 
           key={movie.id}
-          className="row__poster"
-          src={`${base_url}${movie.poster_path}`} alt={movie.name}
+          className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+          src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}
           />
         ))}
         </div>
