3b8c16330a2e489454e5a197946e84d9b737071f
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 10:08:59 +0000

    sent requests for movies in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Building netflix clone today</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
