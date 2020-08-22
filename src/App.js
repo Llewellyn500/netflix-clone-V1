25890fd14d29275fde2e0a1659bbfa2d21dbdcae
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:37:24 +0000

    added the rest of the movies

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Hoeeoe Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
