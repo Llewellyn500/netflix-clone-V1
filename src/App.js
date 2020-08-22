bff1969888f52b40d8308c5c29ba32543d7ed7ca
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:55:24 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <h1>Building netflix clone today</h1>
+     
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
