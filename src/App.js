c3fb12e51c78d401c69b33d1ae81cdc1acf141bd
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:52:18 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <h1>Building netflix clone today</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
