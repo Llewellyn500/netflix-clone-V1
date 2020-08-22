1f1b4e26440b4eb3b8b447cec11ab52df9c4c375
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 10:20:32 +0000

    update Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState} from 'react'
+import axios from './axios';
 
 function Row({ title }) {
+  const [movies, setMovies] = useState([]);
+  
+  // a snippet of code which runs based on a specific condition
+  useEffect(()=> {
+    
+    async function fetchData() {
+    const request = await axios.get(fetchUrl);
+    }
+    fetchData();
+  }, []);
+  
   return (
     <div>
       <h2>{title}</h2>
