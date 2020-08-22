1eb2274678df85b8ecf0e3ba5bfcd974c54f4714
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 17:14:39 +0000

    update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "./Nav.css";
 
 function Nav() {
   return (
       <div className="nav">
         <img
           className="nav_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+          src="https://commons.m.wikimedia.org/wiki/File:Netflix_2015_logo.svg"
           alt="Netflix Logo"
         />
         
