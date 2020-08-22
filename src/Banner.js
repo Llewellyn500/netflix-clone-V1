1ed6105b64e7b78841ff04c375775029f854be4f
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 15:57:15 +0000

    update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -21,8 +21,28 @@ function Banner() {
   console.log(movie);
   
   return (
-    <header>
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+          "https://image.tmdb.org/t/p/original/${movie?.background_path}"
+          )`,
+          backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+      <h1>
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
       
+      <div className="banner__buttons">
+        <button className="banner__button">Play</button>
+        <button className="banner__button">My List</button>
+      </div>
+      
+      <h1 className="banner__description">{movie?.overview}</h1>
+      
+    </div>
     </header>
   )
 }
