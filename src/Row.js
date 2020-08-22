ff56c4cdec548ce448b81fea0a14688d46e65d85
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 10:49:39 +0000

    added movie images to code

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,7 @@
-import React, { useState} from 'react'
+import React, { useState} from 'react';
 import axios from './axios';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title }) {
   const [movies, setMovies] = useState([]);
@@ -8,19 +10,25 @@ function Row({ title }) {
   useEffect(()=> {
     
     async function fetchData() {
-    const request = await axios.get(fetchUrl);
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
   
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
       
-      
-      
+        <div className="row_posters">
+        
+        {movies.map(movie => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+        ))}
+        </div>
     </div>
-    )
+  )
 }
 
 export default Row
