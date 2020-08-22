ed3d0c868533f0582eb50f5632644894258682d8
Llewellyn Paintsil <Llewellynpaintsil34@gmail.com>
2020-08-22 11:23:34 +0000

    update requests.js

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "f39a9cefbf8bce7a9519c244a1350c78";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_jey=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_gnres=28`, 
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_gnres=35`,
